c04ddd71f533b5d93c82b5951cd6eb761414a47b
mrbonk97 <mrbonk97@gmail.com>
2024-12-11 23:40:30 +0900

    첫 페이지 꾸미는 중

---
+++ components/concert/concert-calendar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
+export const ConcertCalendar = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  return (
+    <div>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </div>
+  );
+};
