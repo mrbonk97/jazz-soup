2184e401562e7cf80d0d83eefaa25c87d1dc8c58
mrbonk97 <mrbonk97@gmail.com>
2024-12-11 23:40:10 +0900

    네비게이션 바 구현

---
+++ components/nav/profile-button.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const ProfileButton = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>프로필</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>로그인</DropdownMenuItem>
+        <DropdownMenuItem>회원가입</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
