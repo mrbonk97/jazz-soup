511e9a88ce6f1127c81b542ef3dd0ba588614aa5
mrbonk97 <mrbonk97@gmail.com>
2024-12-18 20:59:48 +0900

    asset 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Topnav } from "@/components/nav/top-nav";
-import { Leftnav } from "@/components/nav/left-nav";
-import { Noto_Sans_KR } from "next/font/google";
 import { Footer } from "@/components/nav/footer";
 
 export const metadata: Metadata = {
   title: "재즈수프",
   description: "서울의 모든 재즈바",
 };
-
-const font = Noto_Sans_KR({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -19,9 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={font.className + " antialiased"}>
+      <body className={"antialiased"}>
         <Topnav />
-        <Leftnav />
         {children}
         <Footer />
       </body>
